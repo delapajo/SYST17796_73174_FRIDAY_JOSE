50660bc251b042376b5d567369937943e46f5e81
migue <migue@LAPTOP-IRVQF72N>
2019-01-25 13:26:26 -0500

    Created gitignore

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,31 @@
+    /*
+        Name:  Jose Miguelle Dela Paz
+        Assignment:  [assignment name]
+        Program: [your program name here]
+        Date:  [assignment due date here]
+    
+        Description:
+        [program description in your own words]
+    */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gittest;
+
+/**
+ *
+ * @author migue
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
